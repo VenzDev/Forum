fba5b2e11f3a0cf7b68014de4d1c0db54016b8d9
VenzDev <kamil.bechta1@gmail.com>
2020-03-19 20:57:27 +0100

    basic structure created

---
+++ server/build/index.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var _dotenv = require("dotenv");
+
+var _dotenv2 = _interopRequireDefault(_dotenv);
+
+var _express = require("express");
+
+var _express2 = _interopRequireDefault(_express);
+
+var _cors = require("cors");
+
+var _cors2 = _interopRequireDefault(_cors);
+
+var _bodyParser = require("body-parser");
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _morgan = require("morgan");
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+var _mongoose = require("mongoose");
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+var _helmet = require("helmet");
+
+var _helmet2 = _interopRequireDefault(_helmet);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dotenv2.default.config();
+
+
+var app = (0, _express2.default)();
+
+app.use((0, _cors2.default)());
+app.use(_bodyParser2.default.json());
+app.use((0, _helmet2.default)());
+app.use((0, _morgan2.default)("dev"));
+
+app.get("/", function (req, res) {
+  res.status(200).json({ message: "Welcome to node js & Express" });
+});
+app.get("/info", function (req, res) {
+  res.status(200).json({ message: "Working!" });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port 3000");
+});
+
+_mongoose2.default.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Mongo database is running!");
+  }
+});
+//# sourceMappingURL=index.js.map
--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+import logger from "morgan";
+import mongoose from "mongoose";
+import helmet from "helmet";
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to node js & Express" });
@@ -15,3 +24,15 @@ app.get("/info", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000");
 });
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Mongo database is running!`);
+    }
+  }
+);
