35f3236c6852d575f55e16a6664ed4ac8fd394d3
VenzDev <kamil.bechta1@gmail.com>
2020-03-25 18:39:03 +0100

    moved dev urls to prod

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -4,8 +4,6 @@ export const registerEndpoint = url + "/register";
 export const loginEndpoint = url + "/login";
 export const authEndpoint = url + "/auth";
 export const searchEndpoint = url + "/search?data=";
-
-//DEV
-export const fetchForumsEndpoint = "http://localhost:3000/getForums";
-export const createThreadEndpoint = "http://localhost:3000/createThread";
-export const findThreadEndpoint = "http://localhost:3000/findThread?id=";
+export const fetchForumsEndpoint = url + "/getForums";
+export const createThreadEndpoint = url + "/createThread";
+export const findThreadEndpoint = url + "/findThread?id=";
