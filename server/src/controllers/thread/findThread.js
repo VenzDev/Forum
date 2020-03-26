129a3d01c7f99c34eb5881ce5bb81f4a078f4493
VenzDev <kamil.bechta1@gmail.com>
2020-03-26 20:59:20 +0100

    small fix in findThread

--- server/src/controllers/thread/findThread.js
+++ server/src/controllers/thread/findThread.js
@@ -4,7 +4,12 @@ import { ErrorHandler } from "../../utils/error";
 export const findThread = async (req, res, next) => {
   try {
     const { id } = req.query;
-    const thread = await Thread.findById(id).populate("posts");
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "posts",
+        populate: { path: "user", select: "_id name surname" }
+      })
+      .populate({ path: "user", select: "_id name surname" });
     if (thread) res.status(201).json(thread);
     else throw new ErrorHandler(422, "Thread not found");
     next();
