96c97e7f61a1279faa09fbec4661212559a5a12b
VenzDev <kamil.bechta1@gmail.com>
2020-05-22 13:17:34 +0200

    changes

--- client/src/components/ForumItem/DeletePopup/index.js
+++ client/src/components/ForumItem/DeletePopup/index.js
@@ -3,14 +3,32 @@ import Popup from "../../Popup";
 import s from "./deletepopup.module.scss";
 import b from "../../button.module.scss";
 import { MdClose } from "react-icons/md";
+import axios from "axios";
+import { forum as forumRedux } from "../../../redux/forum";
+import { useDispatch } from "react-redux";
+import showToast from "../../../utils/showToast";
 
-const DeletePopup = ({ handleClose }) => {
+const DeletePopup = ({ handleClose, forum }) => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    axios.get("http://localhost:3000/deleteForum?id=" + forum._id).then((res) => {
+      if (res.status === 202) {
+        showToast("Forum deleted successfully");
+        dispatch(forumRedux.fetchForums());
+      }
+    });
+  };
   return (
     <Popup>
       <div className={s.popup}>
         <MdClose onClick={handleClose} className={s.closeItem} />
         <p style={{ marginBottom: "3rem" }}>Do you want to delete Forum?</p>
-        <button style={{ padding: "1.5rem", margin: "0 auto" }} className={b.button}>
+        <button
+          onClick={handleSubmit}
+          style={{ padding: "1.5rem", margin: "0 auto" }}
+          className={b.button}
+        >
           Delete
         </button>
       </div>
