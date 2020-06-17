0161872c84fc86ebe2b9142e3bce38345cd5dd2d
VenzDev <kamil.bechta1@gmail.com>
2020-06-18 00:50:37 +0200

    react lazy function for optimalizaiton added

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Wrapper from "./utils/Wrapper";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
 import Header from "./components/Header";
-import forgotPasswordPage from "./pages/ForgotPasswordPage";
-import RegisterPage from "./pages/RegisterPage";
 import { ToastContainer } from "react-toastify";
-import CreateThreadPage from "./pages/CreateThreadPage";
-import ThreadPage from "./pages/ThreadPage";
-import ThreadsPage from "./pages/ThreadsPage";
-import UserPage from "./pages/UserPage";
-import UserThreads from "./pages/UserThreads";
-import ErrorPage from "./pages/ErrorPage";
+import Loader from "react-loader-spinner";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const ForgotPasswordPage = lazy(() => import("./pages/ForgotPasswordPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const CreateThreadPage = lazy(() => import("./pages/CreateThreadPage"));
+const ThreadPage = lazy(() => import("./pages/ThreadPage"));
+const ThreadsPage = lazy(() => import("./pages/ThreadsPage"));
+const UserPage = lazy(() => import("./pages/UserPage"));
+const UserThreads = lazy(() => import("./pages/UserThreads"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
 
 function App() {
   return (
@@ -27,18 +29,34 @@ function App() {
             language-code="en"
           ></df-messenger>
           <ToastContainer />
-          <Switch>
-            <Route exact path="/" component={HomePage}></Route>
-            <Route exact path="/login" component={LoginPage}></Route>
-            <Route exact path="/register" component={RegisterPage}></Route>
-            <Route exact path="/createThread" component={CreateThreadPage}></Route>
-            <Route exact path="/forum/:name" component={ThreadsPage}></Route>
-            <Route exact path="/user/:id" component={UserPage}></Route>
-            <Route exact path="/thread/:id" component={ThreadPage}></Route>
-            <Route exact path="/forgot" component={forgotPasswordPage}></Route>
-            <Route exact path="/yourThreads" component={UserThreads}></Route>
-            <Route component={ErrorPage} />
-          </Switch>
+          <Suspense
+            fallback={
+              <Loader
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  zIndex: "1000",
+                }}
+                type="BallTriangle"
+                color="blue"
+              />
+            }
+          >
+            <Switch>
+              <Route exact path="/" component={HomePage}></Route>
+              <Route exact path="/login" component={LoginPage}></Route>
+              <Route exact path="/register" component={RegisterPage}></Route>
+              <Route exact path="/createThread" component={CreateThreadPage}></Route>
+              <Route exact path="/forum/:name" component={ThreadsPage}></Route>
+              <Route exact path="/user/:id" component={UserPage}></Route>
+              <Route exact path="/thread/:id" component={ThreadPage}></Route>
+              <Route exact path="/forgot" component={ForgotPasswordPage}></Route>
+              <Route exact path="/yourThreads" component={UserThreads}></Route>
+              <Route component={ErrorPage} />
+            </Switch>
+          </Suspense>
         </Wrapper>
       </BrowserRouter>
     </>
